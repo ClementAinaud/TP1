4188145646cfe7280e80cbc64a0703741d614c8d
cleme <cleme@PCAINAUD>
2017-09-12 13:49:45 +0200

    creation du projet

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>On test le tp 1</h1>
+        <h1>On test le tp 1</h1>
+        <h1>On test le tp 1</h1>
+        
+    </body>
+</html>
